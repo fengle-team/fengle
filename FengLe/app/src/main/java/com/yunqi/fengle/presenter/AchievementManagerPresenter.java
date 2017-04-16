480113532c7b364a3a3f6c019cc469ebb1d67fad
axue <376982054@qq.com>
2017-04-16 08:50:53 +0800

    [ghcui]fix bug

--- FengLe/app/src/main/java/com/yunqi/fengle/presenter/AchievementManagerPresenter.java
+++ FengLe/app/src/main/java/com/yunqi/fengle/presenter/AchievementManagerPresenter.java
@@ -1,8 +1,11 @@
 package com.yunqi.fengle.presenter;
 
 import com.yunqi.fengle.base.RxPresenter;
+import com.yunqi.fengle.model.http.RetrofitHelper;
 import com.yunqi.fengle.presenter.contract.AchievementManagerContract;
 import com.yunqi.fengle.presenter.contract.LoginContract;
+
+import javax.inject.Inject;
 
 /**
  * @Author: Huangweicai
@@ -12,6 +15,13 @@ import com.yunqi.fengle.presenter.contract.LoginContract;
 
 public class AchievementManagerPresenter extends RxPresenter<AchievementManagerContract.View> implements AchievementManagerContract.Presenter{
 
+    public String TAG = getClass().getName();
+    private RetrofitHelper mRetrofitHelper;
+
+    @Inject
+    public AchievementManagerPresenter(RetrofitHelper retrofitHelper) {
+        this.mRetrofitHelper = retrofitHelper;
+    }
 
 
 }
